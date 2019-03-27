4d79b82ab9619b168a5457abd6e442b13951790f
hansrobothans <hansrobothans@gmail.com>
2019-03-27 21:47:18 +0800

    Update update.sh

--- shell/git/update.sh
+++ shell/git/update.sh
@@ -1,16 +1,18 @@
 #更新软件源，更新所有软件
-sudo apt update && sudo apt upgrade -y
+sudo apt update
+ sudo apt upgrade -y
 #安装必要软件：vim tmux
 sudo apt install vim tmux -y
 #卸载Ubuntu自带火狐浏览器
 sudo apt-get purge firefox
 #安装Google浏览器
-mkdir ~/google
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb ~/google
-cd ~/google
-sudo dpkg -i google-chrome*
-sudo apt-get install -f
+sudo wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+/usr/bin/google-chrome-stable
+
 #安装atom
-sudo add-apt-repository ppa:webupd8team/atom  
-sudo apt-get update  
-sudo apt-get install atom  
+#sudo add-apt-repository ppa:webupd8team/atom
+#sudo apt-get update
+#sudo apt-get install atom  
